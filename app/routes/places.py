fb3222fada3d54b9013937a5677a6285d7bee56c
Jungminchae <minchae3618@gmail.com>
2022-06-13 21:38:11 +0900

    extract model, label from inference function

--- app/routes/places.py
+++ app/routes/places.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import JSONResponse
-from app.utils.image import preprocess_image
+from app.utils.image import get_place_model, get_place_label_info, preprocess_image
 from app.service.inference import inference, get_specific_label_info
 
 
 router = APIRouter(prefix="/places")
+PLACE_MODEL = get_place_model()
+LABEL_INFO = get_place_label_info()
 
 
 @router.post("/inference/")
 async def infer_place(image: UploadFile = File(...)) -> JSONResponse:
     place_image = await image.read()
     place_image = preprocess_image(place_image)
-    pred_label_index = inference(place_image)
-    inference_results = get_specific_label_info(pred_label_index)
+    pred_label_index = inference(PLACE_MODEL, place_image)
+    inference_results = get_specific_label_info(LABEL_INFO, pred_label_index)
     return JSONResponse(content=inference_results, status_code=201)
--- app/service/inference.py
+++ app/service/inference.py
@@ -2,15 +2,13 @@ import numpy as np
 from app.utils.image import get_place_model, get_place_label_info
 
 
-def inference(image) -> str:
-    model = get_place_model()
+def inference(model, image) -> str:
     pred = model.predict(image, batch_size=1)
     return str(np.argmax(pred))
 
 
-def get_specific_label_info(index: str) -> dict:
-    label_info = get_place_label_info()
-    label_info = label_info[index]
+def get_specific_label_info(label: dict, index: str) -> dict:
+    label_info = label[index]
     label_category = label_info["category"]
     random_label_sentence = np.random.choice(label_info["sentence"])
     return {"label_category": label_category, "sentence": random_label_sentence}
